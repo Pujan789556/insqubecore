beebdce36b2abf89460f6b0b91710b80e959e37b
IP Bastola <ip.bastola@gmail.com>
2016-09-09 17:14:25 +0545

    Rename Insqube DB Helper file

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'form', 'insqb_db');
+$autoload['helper'] = array('url', 'form', 'insqube', 'insqube_db');
 
 /*
 | -------------------------------------------------------------------
