4c782624f34918a7876e63193f10e7a1ab67a724
IP Bastola <ip.bastola@gmail.com>
2016-09-09 17:11:16 +0545

    Add Style: HTML5 On/Off switch (supporting less)

--- public/themes/AdminLTE-2.3.6/Gruntfile.js
+++ public/themes/AdminLTE-2.3.6/Gruntfile.js
@@ -34,7 +34,10 @@ module.exports = function (grunt) {
           "dist/css/skins/skin-green-light.css": "build/less/skins/skin-green-light.less",
           "dist/css/skins/skin-red-light.css": "build/less/skins/skin-red-light.less",
           "dist/css/skins/skin-purple-light.css": "build/less/skins/skin-purple-light.less",
-          "dist/css/skins/_all-skins.css": "build/less/skins/_all-skins.less"
+          "dist/css/skins/_all-skins.css": "build/less/skins/_all-skins.less",
+
+          // On/Off Switch (added by IP Bastola)
+          "dist/css/switch.css": "build/less/switch.less"
         }
       },
       // Production compresses version
