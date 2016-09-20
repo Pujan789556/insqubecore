8027bf83f63ddbd79a848fc1e5e201bdff545455
IP Bastola <ip.bastola@gmail.com>
2016-09-20 22:26:58 +0545

    Add department module

---
+++ application/controllers/Departments.php
@@ -0,0 +1,327 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Departments Controller
+ * 
+ * This controller falls under "Master Setup" category.
+ *  
+ * @category 	Master Setup
+ */
+
+// --------------------------------------------------------------------
+
+class Departments extends MY_Controller
+{
+	/**
+	 * Validation Rules
+	 * 
+	 * @var array
+	 */
+	private $form_elements = [
+		[
+			'name' 		=> 'name',
+	        'label' 	=> 'Department Name',
+	        '_id' 		=> 'name',
+	        '_type' 	=> 'text',
+	        '_required' => true
+		],
+		[
+			'name' 		=> 'code',
+	        'label' 	=> 'Department Code',
+	        '_id' 		=> 'code',
+	        '_type'		=> 'text',
+	        '_required' => true
+		]	
+	];
+
+	// --------------------------------------------------------------------
+
+	function __construct()
+	{
+		parent::__construct();
+		
+		// Only Admin Can access this controller
+		if( !$this->dx_auth->is_admin() )
+		{
+			$this->dx_auth->deny_access();
+		}
+
+		// Form Validation
+		$this->load->library('Form_validation');				
+	
+		// Set Template for this controller
+        $this->template->set_template('dashboard');
+
+        // Basic Data
+        $this->data['site_title'] = 'Master Setup | Departments';
+
+        // Setup Navigation        
+		$this->active_nav_primary([
+			'level_0' => 'master_setup',
+			'level_1' => 'general',
+			'level_2' => $this->router->class
+		]);
+
+		// Load Model
+		$this->load->model('department_model');
+
+		// Load Activitis Library
+		$this->load->library('activity');    
+	}
+	
+	// --------------------------------------------------------------------
+
+	/**
+	 * Default Method
+	 * 
+	 * Render the settings
+	 * 
+	 * @return type
+	 */
+	function index()
+	{
+		/**
+		 * Normal Form Render
+		 */
+		// this will generate cache name: mc_master_departments_all
+		$records = $this->department_model->set_cache('all')->get_all();
+	
+		$this->template->partial(
+							'content_header', 
+							'templates/_common/_content_header',
+							[
+								'content_header' => 'Manage Departments',
+								'breadcrumbs' => ['Master Setup' => NULL, 'Departments' => NULL]
+						])
+						->partial('content', 'setup/departments/_index', compact('records'))
+						->render($this->data);
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Edit a Role
+	 * 
+	 * 
+	 * @param integer $id 
+	 * @return void
+	 */
+	public function edit($id)
+	{
+		// Valid Record ?
+		$id = (int)$id;
+		$record = $this->department_model->get($id);
+		if(!$record)
+		{
+			$this->template->render_404();
+		}
+
+		// Form Submitted? Save the data
+		$json_data = $this->_save('edit', $record);
+		
+
+		// No form Submitted?
+		$json_data['form'] = $this->load->view('setup/departments/_form', 
+			[
+				'form_elements' => $this->form_elements,
+				'record' 		=> $record
+			], TRUE);
+
+		// Return HTML 
+		$this->template->json($json_data);
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Add a new Role
+	 * 
+	 * @return void
+	 */
+	public function add()
+	{
+		$record = NULL;
+
+		// Form Submitted? Save the data
+		$json_data = $this->_save('add');
+
+		
+		// No form Submitted?
+		$json_data['form'] = $this->load->view('setup/departments/_form', 
+			[
+				'form_elements' => $this->form_elements,
+				'record' 		=> $record
+			], TRUE);
+
+		// Return HTML 
+		$this->template->json($json_data);
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Save a Record
+	 * 
+	 * @param string $action [add|edit]
+	 * @param object|null $record Record Object or NULL
+	 * @return array
+	 */
+	private function _save($action, $record = NULL)
+	{
+		// Valid action?
+		if( !in_array($action, array('add', 'edit')))
+		{
+			return [
+				'status' => 'error',
+				'message' => 'Invalid action!'
+			];
+		}
+
+		/**
+		 * Form Submitted?
+		 */
+		$return_data = [];
+
+		if( $this->input->post() )
+		{
+			$done = FALSE;
+			
+
+			// Insert or Update?
+			if($action === 'add')
+			{
+				$done = $this->department_model->from_form()->insert();
+
+				// @NOTE: Activity Log will be automatically inserted
+			}
+			else
+			{
+				// Update Validation Rule on Update
+				$this->department_model->rules['insert'][1]['rules'] = 'trim|required|max_length[5]|callback_check_duplicate';
+				
+				
+				// Now Update Data
+				$done = $this->department_model->from_form()->update(NULL, $record->id) && $this->department_model->log_activity($record->id, 'E');
+			}			
+
+        	if(!$done)
+			{
+				$status = 'error';
+				$message = 'Validation Error.';				
+			}
+			else
+			{
+				$status = 'success';
+				$message = 'Successfully Updated.';				
+			}
+
+			// Success HTML
+			$success_html = '';
+			if($status === 'success' )
+			{
+				if($action === 'add')
+				{
+					$records = $this->department_model->set_cache('all')->get_all();
+					$success_html = $this->load->view('setup/departments/_list', ['records' => $records], TRUE);
+				}
+				else
+				{
+					// Get Updated Record
+					$record = $this->department_model->get($record->id);
+					$success_html = $this->load->view('setup/departments/_single_row', ['record' => $record], TRUE);
+				}
+			}
+
+			$return_data = [
+				'status' 		=> $status,
+				'message' 		=> $message,
+				'reloadForm' 	=> $status === 'error',
+				'hideBootbox' 	=> $status === 'success',
+				'updateSection' => $status === 'success',
+				'updateSectionData'	=> $status === 'success' 
+										? 	[
+												'box' 	=> $action === 'add' 
+															? '#iqb-data-list' 
+															: '#_data-row-' . $record->id,
+												'html' 	=> $success_html,
+
+												//
+												// How to Work with success html?
+												// Jquery Method 	html|replaceWith|append|prepend etc.
+												// 
+												'method' 	=> $action === 'add' ? 'html' : 'replaceWith'
+											]
+										: NULL,
+				'form' 	  		=> $status === 'error' 
+									? 	$this->load->view('setup/departments/_form', 
+											[
+												'form_elements' => $this->form_elements,
+												'record' 		=> $record
+											], TRUE)
+									: 	null
+
+			];
+		}
+
+		return $return_data;
+	}
+
+	// --------------------------------------------------------------------
+
+	public function delete($id)
+	{
+		// Valid Record ?
+		$id = (int)$id;
+		$record = $this->department_model->get($id);
+		if(!$record)
+		{
+			$this->template->render_404();
+		}
+
+		// Admin Constraint?
+		$done = $this->department_model->delete($record->id);
+		if($done)
+		{
+			$data = [
+				'status' 	=> 'success',
+				'message' 	=> 'Successfully deleted!',
+				'removeRow' => true,
+				'rowId'		=> '#_data-row-'.$record->id
+			];
+		}
+		else
+		{
+			$data = [
+				'status' 	=> 'error',
+				'message' 	=> 'Could not be deleted. It might have references to other module(s)/component(s).'
+			];
+		}
+		return $this->template->json($data);
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+     * Check Duplicate Callback
+     * 
+     * @param string $code 
+     * @param integer|null $id 
+     * @return bool
+     */	
+    public function check_duplicate($code, $id=NULL){
+
+    	$code = strtoupper( $code ? $code : $this->input->post('code') );
+    	$id   = $id ? (int)$id : (int)$this->input->post('id');
+
+        if( $this->department_model->check_duplicate(['code' => $code], $id))
+        {
+            $this->form_validation->set_message('check_duplicate', 'The %s already exists.');
+            return FALSE;
+        }
+        return TRUE;
+    }
+
+    // --------------------------------------------------------------------
+
+}
---
+++ application/views/setup/departments/_form.php
@@ -0,0 +1,65 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Form : Department
+ */
+?>
+<?php echo form_open( $this->uri->uri_string(), 
+                        [
+                            'class' => 'form-horizontal form-iqb-general',
+                            'data-pc' => '.bootbox-body' // parent container ID
+                        ], 
+                        // Hidden Fields
+                        isset($record) ? ['id' => $record->id] : []); ?>
+    
+    <?php foreach($form_elements as $element):?>        
+        <div class="form-group <?php echo form_error($element['name']) ? 'has-error' : '';?>">
+            <label for="<?php echo $element['_id'] ?>" class="col-sm-2 control-label"><?php echo $element['label'] . field_compulsary_text( $element['_required']);?></label>
+            <div class="col-sm-10">
+                <?php 
+                /**
+                 * Load Form Element
+                 */
+                $element_config = array(
+                    'name'          => $element['name'],
+                    'id'            => $element['_id'],
+                    'class'         => 'form-control',
+                    'placeholder'   => $element['label']
+                );
+                $value = set_value($element['name']) ? set_value($element['name'], '', FALSE) : ( isset($record) ? $record->{$element['name']} : '' );
+
+                switch($element['_type'])
+                {
+                    case 'text':
+                        echo form_input($element_config, $value);
+                        break;
+
+                    case 'email':
+                        echo form_email($element_config, $value);
+                        break;
+
+                    case 'url':
+                        echo form_url($element_config, $value);
+                        break;
+
+                    case 'textarea':
+                        echo form_textarea($element_config, $value);
+                        break;
+
+                    case 'dropdown':
+                        echo form_dropdown($element_config, $element['_data'], $value);
+                        break;
+
+                    case 'switch':
+                        $element_config['class'] = 'switch-checkbox';
+                        $element_config['switch-type'] = 'switch-primary';
+                        echo form_switch($element_config, $element['_data'], set_value($element['name']) || $record->{$element['name']});
+                        break;
+                }
+                ?>
+                <?php if(form_error($element_config['name'])):?><span class="help-block"><?php echo form_error($element_config['name']); ?></span><?php endif?>
+            </div>
+        </div>
+    <?php endforeach?>   
+    <button type="submit" class="hide">Submit</button> 
+<?php echo form_close();?>
