b212921bbefefe9a029f669e589dcfe6bd79743d
IP Bastola <ip.bastola@gmail.com>
2017-01-10 14:19:34 +0545

    Fix - Issue #2

--- application/views/objects/forms/_form_object_motor.php
+++ application/views/objects/forms/_form_object_motor.php
@@ -35,50 +35,58 @@ $this->load->view('templates/_common/_form_components_horz', [
 ?>
 
 <script type="text/javascript">
-function _po_to_be_intimated(d, et){
-    var $this       = $(d), // the source element
-    $reg_box        = $('#_motor-registration-no').closest('.form-group'),
-    $reg_date_box   = $('#_motor-registration-date').closest('.form-group'),
-    $reg_field      = $('#_motor-registration-no'),
-    $reg_date_field = $('#_motor-registration-date');
 
-    if(et === 'ifChecked' || $this.prop('checked') === true){
-        $reg_field.val('TO BE INTIMATED');
-        $reg_date_field.val('');
+    function _po_to_be_intimated(d, et){
+        var $this       = $(d), // the source element
+        $reg_box        = $('#_motor-registration-no').closest('.form-group'),
+        $reg_date_box   = $('#_motor-registration-date').closest('.form-group'),
+        $reg_field      = $('#_motor-registration-no'),
+        $reg_date_field = $('#_motor-registration-date');
 
-        $reg_box.hide(500);
-        $reg_date_box.hide(500);
-    }else{
-        $reg_box.show(500);
-        $reg_date_box.show(500);
+        if(et === 'ifChecked' || $this.prop('checked') === true){
+            $reg_field.val('TO BE INTIMATED');
+            $reg_date_field.val('');
+
+            $reg_box.hide(500);
+            $reg_date_box.hide(500);
+        }else{
+            $reg_box.show(500);
+            $reg_date_box.show(500);
+        }
     }
-}
 
-function _po_motor_change_sub_portfolio(d){
-    var $this = $(d),
-        v = $this.val();
-    if(v == 'CVC')
-    {
-        $('#_motor-vehicle-df').closest('.form-group').hide();
-        $('#_motor-vehicle-cvc-type').closest('.form-group').show();
-    }else if(v == 'MCY'){
-        $('#_motor-vehicle-cvc-type').closest('.form-group').hide();
-        $('#_motor-vehicle-df').closest('.form-group').show();
-    }else{
-        $('#_motor-vehicle-cvc-type').closest('.form-group').hide();
-        $('#_motor-vehicle-df').closest('.form-group').hide();
+    function _po_motor_change_sub_portfolio(d){
+        var $this = $(d),
+            v = $this.val();
+        if(v == 'CVC')
+        {
+            $('#_motor-vehicle-df').closest('.form-group').hide();
+            $('#_motor-vehicle-cvc-type').closest('.form-group').show();
+        }else if(v == 'MCY'){
+            $('#_motor-vehicle-cvc-type').closest('.form-group').hide();
+            $('#_motor-vehicle-df').closest('.form-group').show();
+        }else{
+            $('#_motor-vehicle-cvc-type').closest('.form-group').hide();
+            $('#_motor-vehicle-df').closest('.form-group').hide();
+        }
+
+        // Default Engine Capacity and Carrying Capacity Unit ( Motorcycle & Private Vehicle -> CC | Seat )
+        if( v !== '' &&  v !== 'CVC'){
+            $('#_motor-vehicle-ec-unit').val('CC');
+            $('#_motor-vehicle-carrying-unit').val('S');
+        }
     }
-}
 
-(function($){
+    (function($){
 
-    // Subportfolio Change
-    _po_motor_change_sub_portfolio('#_motor-sub-portfolio');
+        // Subportfolio Change
+        _po_motor_change_sub_portfolio('#_motor-sub-portfolio');
 
-    // To Be Intimated Toggle
-    _po_to_be_intimated('#_motor-vehicle-to-be-intimated');
-    $('#_motor-vehicle-to-be-intimated').on('ifChecked ifUnchecked', function(event){
-        _po_to_be_intimated(this, event.type);
-    });
-})(jQuery);
+        // To Be Intimated Toggle
+        _po_to_be_intimated('#_motor-vehicle-to-be-intimated');
+        $('#_motor-vehicle-to-be-intimated').on('ifChecked ifUnchecked', function(event){
+            _po_to_be_intimated(this, event.type);
+        });
+
+    })(jQuery);
 </script>
