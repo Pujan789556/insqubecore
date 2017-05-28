c87f1abb8fdd7b97ce2460575631b4f82fc75874
ip bastola <ip.bastola@gmail.com>
2017-05-28 15:58:14 +0545

    Update - AC Groups - Prevent first 200 Records from delete

--- web/application/models/Ac_account_group_model.php
+++ web/application/models/Ac_account_group_model.php
@@ -29,7 +29,7 @@ class Ac_account_group_model extends MY_Model
      * Protect Default Records?
      */
     public static $protect_default = TRUE;
-    public static $protect_max_id = 5; // Prevent first 500 records from deletion.
+    public static $protect_max_id = 200; // Prevent first 500 records from deletion.
 
 	// --------------------------------------------------------------------
 
