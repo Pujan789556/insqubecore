f7783eb00095f622b2d22e898026e94300d5c3f4
ip bastola <ip.bastola@gmail.com>
2017-09-04 16:36:34 +0545

    Update Object Popup to accomodate Fire portfolio

--- web/application/views/objects/snippets/_popup.php
+++ web/application/views/objects/snippets/_popup.php
@@ -5,11 +5,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 
 /**
- * MOTOR
- * -----
+ * MOTOR PORTFOLIOS
+ * ----------------
  * For all type of motor portfolios, we have same snippet
  */
 if( in_array($record->portfolio_id, array_keys(IQB_PORTFOLIO__SUB_PORTFOLIO_LIST__MOTOR)) )
 {
 	$this->load->view('objects/snippets/_popup_motor', ['record' => $record]);
+}
+
+/**
+ * FIRE PORTFOLIOS
+ * ----------------
+ */
+else if( in_array($record->portfolio_id, array_keys(IQB_PORTFOLIO__SUB_PORTFOLIO_LIST__FIRE)) )
+{
+	$this->load->view('objects/snippets/_popup_fire', ['record' => $record]);
 }
