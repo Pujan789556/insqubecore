4b835706d97aa55285e16c53be960e812692013e
ip bastola <ip.bastola@gmail.com>
2017-12-15 20:45:40 +0545

    Add Bash Script - CLI - crontab forex

---
+++ scripts/cli.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# CLI Cron JOBs
+#
+# @author 	IP Bastola < ip.bastola@gmail.com>
+#
+# Example Usage (cront tab record) - Every 07:30 AM
+#
+# 				30 7 * * * sh /home/insqube/repo/insqube-core/cli.sh
+#
+
+WEB_ROOT=/var/www/html/neco.insqube.local/web
+
+#
+# Go to web root folder
+#
+cd $WEB_ROOT
+
+#
+# Import Forex Data from NRB
+#
+echo 'Importing forex from NRB ...'
+index.php cli import_forex_rates
+
+
+
+
+#
+## Now Send Email Notification of this job
+#
+# Attachments:
+# 	~/logs/import/17d-import-YYYY-MM-DD-HMS.log
+# 	~/logs/cron/cron-17d-"`date +\%Y-\%m-\%d`".log
+#
+# _yesterday=$(date -d "yesterday" +%Y-%m-%d)
+# _cron_log_file=~/logs/cron/cron-17d-"`date +\%Y-\%m-\%d`".log
+# echo "Please find the attachments." | mutt -s "[NHRPMIS - Phase III - Daily Import Report] $_today"  -a $_logfile -a $_cron_log_file -- dipeshraja@gmail.com ip.bastola@gmail.com gautamxpratik@gmail.com pujan789556@gmail.com
+
