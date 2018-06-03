be992d42382e945db5691b1abb83bd616ba36dac
ip bastola <ip.bastola@gmail.com>
2018-06-03 11:22:07 +0545

    Migration - Item Format - ENG -CAR

--- web/application/models/migrations/M20180530_model.php
+++ web/application/models/migrations/M20180530_model.php
@@ -27,10 +27,82 @@ class M20180530_model extends MY_Model
         /**
          * Migrate Portfolio Object Attributes
          */
-        $this->motor_mcy();
+        $this->eng_car();
 
 
 
+    }
+
+    // ----------------------------------------------------------------
+
+    public function eng_car()
+    {
+        $this->load->helper('ph_eng_car');
+
+        $list = $this->db->select('id, portfolio_id, attributes, amt_sum_insured, si_breakdown')
+                        ->from('dt_objects')
+                        ->where('portfolio_id', IQB_SUB_PORTFOLIO_ENG_CAR_ID)
+                        ->get()->result();
+
+        $item_rules = _OBJ_ENG_CAR_validation_rules(IQB_SUB_PORTFOLIO_ENG_CAR_ID)['items'];
+
+        $total_success = 0;
+
+
+        // Use automatic transaction
+        $this->db->trans_start();
+
+
+            foreach($list as $record)
+            {
+                $items_formatted    = [];
+                $attributes         = json_decode($record->attributes, TRUE);
+
+                if( isset($attributes['items']['sum_insured']) && is_array($attributes['items']['sum_insured']) )
+                {
+                    $items = $attributes['items'];
+                    for($i=0; $i < count($attributes['items']['sum_insured']); $i++)
+                    {
+                        $single = [];
+                        foreach($item_rules as $rule)
+                        {
+                            $key = $rule['_key'];
+                            $single[$key] = $items[$key][$i] ?? '';
+                        }
+                        $items_formatted[] = $single;
+                    }
+
+                    if($items_formatted)
+                    {
+                        $attributes['items'] = $items_formatted;
+
+                        $done = $this->db->where('id', $record->id)
+                                     ->set([
+                                            'attributes' => json_encode($attributes),
+                                            'updated_at' => date('Y-m-d H:i:s'),
+                                            'updated_by' => 1
+                                        ])
+                                     ->update('dt_objects');
+
+                        $done ? $total_success++ : '';
+                    }
+
+                }
+            }
+
+        // Commit all transactions on success, rollback else
+        $this->db->trans_complete();
+
+        // Check Transaction Status
+        if ($this->db->trans_status() === FALSE)
+        {
+            // incomplete message
+            die('Could not migrate.' . PHP_EOL );
+        }
+        else
+        {
+            die("Successfully migrated {$total_success}" . PHP_EOL );
+        }
     }
 
     // ----------------------------------------------------------------
@@ -89,7 +161,6 @@ class M20180530_model extends MY_Model
     }
 
     // ----------------------------------------------------------------
-
 
     public function eng_bl()
     {
