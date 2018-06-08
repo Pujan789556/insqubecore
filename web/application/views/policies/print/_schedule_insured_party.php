680f16e720719f2a048b29133f491609765f2099
ip bastola <ip.bastola@gmail.com>
2018-06-08 12:56:44 +0545

    Update - Schedule insured party info

--- web/application/views/policies/print/_schedule_insured_party.php
+++ web/application/views/policies/print/_schedule_insured_party.php
@@ -10,11 +10,6 @@ $financer_title         = $lang == 'np' ? 'बैंक वा वित्त �
 $other_financer_title   = $lang == 'np' ? 'अरु बैंक वा वित्त कम्पनीको विवरण'        : 'Other Financer(s)';
 $care_of_title          = $lang == 'np' ? 'मार्फत'                  : 'Care Of';
 
-?>
-<strong><?php echo $insured_title ?></strong><br/>
-<?php
-echo htmlspecialchars($record->customer_name) ,
-        '<br/>' , get_contact_widget($record->customer_contact, true, true), '<br/>';
 
 /**
  * Policy Financed?
@@ -37,8 +32,13 @@ if($record->flag_on_credit === 'Y')
             nl2br(htmlspecialchars($record->other_creditors))
         ]);
     }
-
-    echo implode('<br/>', $financer_info), '<br/>';
+    echo implode('<br/>', $financer_info), '<br/><br/>';
 }
+?>
+<strong><?php echo $insured_title ?></strong><br/>
+<?php
+echo htmlspecialchars($record->customer_name) ,
+        '<br/>' , get_contact_widget($record->customer_contact, true, true);
+
 echo  $record->care_of ? '<br/><strong>'.$care_of_title.'</strong><br>' . nl2br(htmlspecialchars($record->care_of)) . '<br/>' : '';
 ?>
