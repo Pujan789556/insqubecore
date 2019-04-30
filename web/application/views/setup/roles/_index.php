5f4b90eb19e6de14cb32ca2f259171e4ce566ca9
Pujan Poudel <pujan@syntegrate.com.np>
2019-04-30 23:28:39 +0545

    Viewbase variable added on Roles

--- web/application/views/setup/roles/_index.php
+++ web/application/views/setup/roles/_index.php
@@ -37,7 +37,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				/**
 				 * Load Rows from View
 				 */
-				$this->load->view('setup/roles/_list');
+				$this->load->view($this->data['_view_base'] . '/_list');
 				?>
 			</div>
 			<!-- /.box-body -->
