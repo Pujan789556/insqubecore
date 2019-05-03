025ad82185f2ce84e39920b487090ff826366182
ip bastola <ip.bastola@gmail.com>
2019-05-03 12:30:17 +0545

    Fix - Forex - Missing

--- web/application/views/setup/forex/_list.php
+++ web/application/views/setup/forex/_list.php
@@ -4,16 +4,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 * Forex:  Data List
 */
 ?>
-<table class="table table-hover" id="search-result-forex">
-	<tr>
-		<th>ID</th>
-		<th>Exchange Date</th>
-		<th>Actions</th>
-	</tr>
-	<?php
-	/**
-	 * Load Rows & Next Link (if any)
-	 */
-	$this->load->view($this->data['_view_base'] . '/_rows');
-	?>
+<table class="table table-hover" id="<?php echo $DOM_RowBoxId ?>">
+	<thead>
+		<tr>
+			<th>ID</th>
+			<th>Exchange Date</th>
+			<th>Actions</th>
+		</tr>
+	</thead>
+	<tbody id="<?php echo $DOM_RowBoxId ?>">
+		<?php
+		/**
+		 * Load Rows & Next Link (if any)
+		 */
+		$this->load->view( $this->data['_view_base'] . '/_rows' );
+		?>
+	</tbody>
 </table>
