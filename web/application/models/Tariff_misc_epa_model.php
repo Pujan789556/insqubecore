f4285072331d928d906d18bd638b3b6a142b7e9a
ip bastola <ip.bastola@gmail.com>
2019-05-03 13:21:47 +0545

    Implement Audit Log - EPA Tariff

--- web/application/models/Tariff_misc_epa_model.php
+++ web/application/models/Tariff_misc_epa_model.php
@@ -11,6 +11,8 @@ class Tariff_misc_epa_model extends MY_Model
 
     protected $log_user = true;
 
+    protected $audit_log = TRUE;
+
     protected $protected_attributes = ['id'];
 
     protected $after_insert  = ['clear_cache'];
