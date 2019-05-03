3928849ee38543cc47b108bfa3485a16c887e931
ip bastola <ip.bastola@gmail.com>
2019-05-03 12:46:08 +0545

    FIX - Users - Missing DOM_RowBoxId

--- web/application/views/setup/users/_list.php
+++ web/application/views/setup/users/_list.php
@@ -4,22 +4,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 * Users:  Data List
 */
 ?>
-<table class="table table-hover" id="search-result-user">
-	<tr>
-		<th>ID</th>
-		<th>Username</th>
-		<th>Role</th>
-		<th>Email</th>
-		<th>Department</th>
-		<th>Branch</th>
-		<th>Fullname</th>
-		<th>Banned</th>
-		<th>Actions</th>
-	</tr>
-	<?php
-	/**
-	 * Load Rows & Next Link (if any)
-	 */
-	$this->load->view($this->data['_view_base'] . '/_rows');
-	?>
+<table class="table table-hover">
+	<thead>
+		<tr>
+			<th>ID</th>
+			<th>Username</th>
+			<th>Role</th>
+			<th>Email</th>
+			<th>Department</th>
+			<th>Branch</th>
+			<th>Fullname</th>
+			<th>Banned</th>
+			<th>Actions</th>
+		</tr>
+	</thead>
+
+	<tbody id="<?php echo $DOM_RowBoxId ?>">
+		<?php
+		/**
+		 * Load Rows & Next Link (if any)
+		 */
+		$this->load->view($this->data['_view_base'] . '/_rows');
+		?>
+	</tbody>
 </table>
