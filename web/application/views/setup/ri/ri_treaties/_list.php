5c50881a9973f4c0faf635c6f7be183207853b24
ip bastola <ip.bastola@gmail.com>
2019-05-03 12:43:02 +0545

    FIX - RI Treaties - Missing

--- web/application/views/setup/ri/ri_treaties/_list.php
+++ web/application/views/setup/ri/ri_treaties/_list.php
@@ -4,19 +4,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 * Setup - RI - Treaties :  Data List
 */
 ?>
-<table class="table table-hover" id="search-result-ri-setup-treaty">
-	<tr>
-		<th>ID</th>
-		<th>Title</th>
-		<th>For</th>
-		<th>Fiscal Year</th>
-		<th>Treaty Type</th>
-		<th>Actions</th>
-	</tr>
-	<?php
-	/**
-	 * Load Rows & Next Link (if any)
-	 */
-	$this->load->view($this->data['_view_base'] . '/_rows');
-	?>
+<table class="table table-hover">
+	<thead>
+		<tr>
+			<th>ID</th>
+			<th>Title</th>
+			<th>For</th>
+			<th>Fiscal Year</th>
+			<th>Treaty Type</th>
+			<th>Actions</th>
+		</tr>
+	</thead>
+
+	<tbody id="<?php echo $DOM_RowBoxId ?>">
+		<?php
+		/**
+		 * Load Rows & Next Link (if any)
+		 */
+		$this->load->view($this->data['_view_base'] . '/_rows');
+		?>
+	</tbody>
 </table>
