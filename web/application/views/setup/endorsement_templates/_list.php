59098bd7aab7589b1b4f9e20df5b841f5d6a7a95
ip bastola <ip.bastola@gmail.com>
2019-05-03 12:39:42 +0545

    FIX - Endorsement Templates - Missing

--- web/application/views/setup/endorsement_templates/_list.php
+++ web/application/views/setup/endorsement_templates/_list.php
@@ -4,18 +4,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 * Endorsement Template:  Data List
 */
 ?>
-<table class="table table-hover" id="search-result-endorsement_templates">
-	<tr>
-		<th>ID</th>
-		<th>Portfolio</th>
-		<th>Type</th>
-		<th>Title</th>
-		<th>Actions</th>
-	</tr>
-	<?php
-	/**
-	 * Load Rows & Next Link (if any)
-	 */
-	$this->load->view($this->data['_view_base'] . '/_rows');
-	?>
+<table class="table table-hover">
+	<thead>
+		<tr>
+			<th>ID</th>
+			<th>Portfolio</th>
+			<th>Type</th>
+			<th>Title</th>
+			<th>Actions</th>
+		</tr>
+	</thead>
+	<tbody id="<?php echo $DOM_RowBoxId ?>">
+		<?php
+		/**
+		 * Load Rows & Next Link (if any)
+		 */
+		$this->load->view($this->data['_view_base'] . '/_rows');
+		?>
+	</tbody>
 </table>
