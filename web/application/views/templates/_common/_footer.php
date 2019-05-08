2c7d7257e8415007fd7772bac4fd9dacaf554b81
ip bastola <ip.bastola@gmail.com>
2019-05-08 11:57:31 +0545

    Fixes insqube-bugs/issues/58

--- web/application/views/templates/_common/_footer.php
+++ web/application/views/templates/_common/_footer.php
@@ -3,5 +3,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');?>
 <div class="pull-right hidden-xs">
   <b>Version</b> 1.0.0
 </div>
-<strong>Copyright &copy; 2014-2016 <a href="http://www.insqube.com" target="_blank">InsQube</a>.</strong> All rights
+<strong>Copyright &copy; <?php echo date('Y') ?> <a href="http://www.insqube.com" target="_blank">InsQube</a>.</strong> All rights
 reserved.
