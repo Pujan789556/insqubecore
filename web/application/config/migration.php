f1403065b281ffbda1deb17de332f70591ef5bb9
ip bastola <ip.bastola@gmail.com>
2019-05-13 19:20:21 +0545

    add migration - object schema upgrade

--- web/application/config/migration.php
+++ web/application/config/migration.php
@@ -69,7 +69,7 @@ $config['migration_auto_latest'] = FALSE;
 | be upgraded / downgraded to.
 |
 */
-$config['migration_version'] = 20190513143847;
+$config['migration_version'] = 20190513191314;
 
 /*
 |--------------------------------------------------------------------------
